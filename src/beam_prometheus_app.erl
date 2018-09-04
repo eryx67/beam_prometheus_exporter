4a848d335554e562e96cc09bc133cd6dd177b987
eryx67 <eryx67@gmail.com>
2018-09-04 15:25:23 +0500

    stop smoothly

--- src/beam_prometheus_app.erl
+++ src/beam_prometheus_app.erl
@@ -6,15 +6,16 @@
 -export([start/2,
          stop/1]).
 
+-define(COWBOY_LISTENER, beam_prometheus_http_listener).
 %% API
 
 start(_StartType, _StartArgs) ->
   Dispatch = cowboy_router:compile([{'_', [{"/metrics/[:registry]", beam_metrics_handler, []}]}]),
   Port = application:get_env(beam_prometheus, port, 9101),
-  {ok, _} = cowboy:start_clear(my_http_listener,
+  {ok, _} = cowboy:start_clear(?COWBOY_LISTENER,
                                [{port, Port}],
                                 #{env => #{dispatch => Dispatch}}),
   beam_prometheus_sup:start_link().
 
 stop(_State) ->
-  ok.
+    ok = cowboy:stop_listener(?COWBOY_LISTENER).
